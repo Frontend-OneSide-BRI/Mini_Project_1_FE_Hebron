500ca063185a4941026d98999a75dc2f4d0efd42
hebron <fidilius.siregar@gmail.com>
2023-05-31 23:16:38 +0700

    feat: add filter by search

--- src/js/script-gallery.js
+++ src/js/script-gallery.js
@@ -24,55 +24,95 @@ fetch("./src/data/data.json")
 
 //   filter
 const categoryOption = document.querySelectorAll(".category-option")
+const form = document.querySelector(".form")
 
 categoryOption.forEach((cat) => {
   cat.addEventListener("click", (e) => {
     e.preventDefault()
     categoryContainer.innerHTML = ""
 
-    fetch("./src/data/data.json")
-      .then((res) => res.json())
-      .then((data) => {
-        if(cat.dataset.category === 'all') {
-          data
-          .map((d) => {
-            const div = document.createElement("div")
-            div.className = "col-lg-3 col-md-4 col-sm-6 col-12 "
-
-            div.innerHTML = `<div class="category-item w-100 ratio ratio-1x1">
-                <img
-                src=${d.image}
-                alt=""
-                class="w-100"
-                />
-                <div class="shadow-item">
-                <h5>${d.title}</h5>
-                </div>
-            </div>`
-
-            categoryContainer.appendChild(div)
-          })
-        }
-
-        data
-          .filter((d) => d.category === cat.dataset.category)
-          .map((d) => {
-            const div = document.createElement("div")
-            div.className = "col-lg-3 col-md-4 col-sm-6 col-12 "
-
-            div.innerHTML = `<div class="category-item w-100 ratio ratio-1x1">
-                <img
-                src=${d.image}
-                alt=""
-                class="w-100"
-                />
-                <div class="shadow-item">
-                <h5>${d.title}</h5>
-                </div>
-            </div>`
-
-            categoryContainer.appendChild(div)
-          })
-      })
+    filterByCategory(cat)
   })
 })
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault()
+  categoryContainer.innerHTML = ""
+  const input = form.querySelector("input")
+
+  filterBySearch(input.value)
+})
+
+function filterBySearch(keyword) {
+  fetch("./src/data/data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      data
+        .filter((d) => d.category.toLowerCase().includes(keyword) || d.title.toLowerCase().includes(keyword))
+        .map((d) => {
+          const div = document.createElement("div")
+          div.className = "col-lg-3 col-md-4 col-sm-6 col-12 "
+
+          div.innerHTML = `<div class="category-item w-100 ratio ratio-1x1">
+            <img
+              src=${d.image}
+              alt=""
+              class="w-100"
+            />
+            <div class="shadow-item">
+              <h5>${d.title}</h5>
+            </div>
+          </div>`
+
+          categoryContainer.appendChild(div)
+        })
+    })
+}
+
+function filterByCategory(category) {
+  fetch("./src/data/data.json")
+    .then((res) => res.json())
+    .then((data) => {
+      if (category.dataset.category === "all") {
+        data.map((d) => {
+          const div = document.createElement("div")
+          div.className = "col-lg-3 col-md-4 col-sm-6 col-12 "
+
+          div.innerHTML = `<div class="category-item w-100 ratio ratio-1x1">
+            <img
+            src=${d.image}
+            alt=""
+            class="w-100"
+            />
+            <div class="shadow-item">
+            <h5>${d.title}</h5>
+            </div>
+        </div>`
+
+          categoryContainer.appendChild(div)
+        })
+
+        return
+      }
+
+      data
+        .filter((d) => d.category === category.dataset.category)
+        .map((d) => {
+          const div = document.createElement("div")
+          div.className = "col-lg-3 col-md-4 col-sm-6 col-12 "
+
+          div.innerHTML = `<div class="category-item w-100 ratio ratio-1x1">
+            <img
+            src=${d.image}
+            alt=""
+            class="w-100"
+            />
+            <div class="shadow-item">
+            <h5>${d.title}</h5>
+            </div>
+        </div>`
+
+          categoryContainer.appendChild(div)
+        })
+    })
+}
